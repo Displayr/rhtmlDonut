8182fb5bb97ded45702f36041679daf7121f4ccb
xtmwang <xtmwang@gmail.com>
2016-04-05 10:31:43 +1000

    include data properly

--- examples/dev.R
+++ examples/dev.R
@@ -1,28 +1,32 @@
 # library(devtools)
 # install_github("xtmwang/DetailedDonutPlot")
+# browser = as.data.frame(data.table::fread("data/browser_version.csv", header = T, sep = ","))
+# values = as.numeric(browser[1,-1])
+# labels = colnames(browser)[-1]
+# groups = as.character(browser[2,-1])
+#
+# qColors1 <- c(grDevices::rgb(91, 155, 213, 255, max = 255), # blue
+#              grDevices::rgb(237, 125, 49, 255, max = 255), # orange
+#              grDevices::rgb(165, 165, 165, 255, max = 255), # grey
+#              grDevices::rgb(30, 192, 0, 255, max = 255), # yelow
+#              grDevices::rgb(68, 114, 196, 255, max = 255), # darker blue
+#              grDevices::rgb(112, 173, 71, 255, max = 255), # green
+#              grDevices::rgb(37, 94, 145, 255, max = 255), # even darker blue
+#              grDevices::rgb(158, 72, 14, 255, max = 255), # blood
+#              grDevices::rgb(99, 99, 99, 255, max = 255), # dark grey
+#              grDevices::rgb(153, 115, 0, 255, max = 255), # brown
+#              grDevices::rgb(38, 68, 120, 255, max = 255), # very dark blue
+#              grDevices::rgb(67, 104, 43, 255, max = 255), # darker green
+#              grDevices::rgb(255, 255, 255, 255, max = 255), # black
+#              grDevices::rgb(255, 35, 35, 255, max = 255)) # red
+# qColors = substring(qColors,1,7)
+# save(values, labels, groups, qColors, file = 'data/browser.rda')
 
-browser = as.data.frame(data.table::fread("examples/browser_version.csv", header = T, sep = ","))
-values = as.numeric(browser[13,-1])
-labels = colnames(browser)[-1]
-groups = strsplit(labels, " ")
-groups = sapply(groups, function(x){x[1]})
+data("browser", package = "DetailedDonutPlot")
+DetailedDonutPlot::detailed.donut(values = values, labels = labels, groups = groups, groups.color = qColors)
 
-qColors <- c(grDevices::rgb(91, 155, 213, 255, max = 255), # blue
-             grDevices::rgb(237, 125, 49, 255, max = 255), # orange
-             grDevices::rgb(165, 165, 165, 255, max = 255), # grey
-             grDevices::rgb(30, 192, 0, 255, max = 255), # yelow
-             grDevices::rgb(68, 114, 196, 255, max = 255), # darker blue
-             grDevices::rgb(112, 173, 71, 255, max = 255), # green
-             grDevices::rgb(37, 94, 145, 255, max = 255), # even darker blue
-             grDevices::rgb(158, 72, 14, 255, max = 255), # blood
-             grDevices::rgb(99, 99, 99, 255, max = 255), # dark grey
-             grDevices::rgb(153, 115, 0, 255, max = 255), # brown
-             grDevices::rgb(38, 68, 120, 255, max = 255), # very dark blue
-             grDevices::rgb(67, 104, 43, 255, max = 255), # darker green
-             grDevices::rgb(255, 255, 255, 255, max = 255), # black
-             grDevices::rgb(255, 35, 35, 255, max = 255)) # red
-colorVec = substring(qColors,1,7)
+# load("data/browser.rda")
+# DetailedDonutPlot::detailed.donut(values = values, labels = labels, groups = groups, groups.color = qColors)
 
-DetailedDonutPlot::detailed.donut(values = values, labels = labels, groups = groups, groups.color = colorVec)
 # DetailedDonutPlot::detailed.donut(values = values, labels = labels, groups = groups, groups.color = colorVec, order = "alphabetical")
 # DetailedDonutPlot::detailed.donut(values = values, labels = labels, groups = groups)
