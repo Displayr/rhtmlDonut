6a32126da51a0f36b468082f6cd5be515fef0280
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2020-07-29 10:41:48 +0800

    code: lint: math.jest.test.js

--- theSrc/scripts/lib/d3pie/math.jest.test.js
+++ theSrc/scripts/lib/d3pie/math.jest.test.js
@@ -1,15 +1,15 @@
-const math = require( './math')
+const math = require('./math')
 
 describe('math.angleAbsoluteDifference', () => {
   it('simple', () => {
-    expect(math.angleAbsoluteDifference(1,2)).toEqual(1)
-    expect(math.angleAbsoluteDifference(1.5,6.7)).toEqual(5.2)
-    expect(math.angleAbsoluteDifference(10,20)).toEqual(10)
-    expect(math.angleAbsoluteDifference(20,10)).toEqual(10)
+    expect(math.angleAbsoluteDifference(1, 2)).toEqual(1)
+    expect(math.angleAbsoluteDifference(1.5, 6.7)).toEqual(5.2)
+    expect(math.angleAbsoluteDifference(10, 20)).toEqual(10)
+    expect(math.angleAbsoluteDifference(20, 10)).toEqual(10)
   })
 
   it('wrapping', () => {
-    expect(math.angleAbsoluteDifference(350,10)).toEqual(20)
+    expect(math.angleAbsoluteDifference(350, 10)).toEqual(20)
     expect(math.angleAbsoluteDifference(10, 350)).toEqual(20)
     expect(math.angleAbsoluteDifference(0, 350)).toEqual(10)
     expect(math.angleAbsoluteDifference(0, 10)).toEqual(10)
