bd342bd88856d4e2cc722b3d21486939f1579a7c
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2020-09-19 09:24:13 +1000

    code: optimise shrinkFontSizesUntilLabelsFitCanvasVertically to not run when minfont=maxfont

--- theSrc/scripts/lib/d3pie/labellers/segmentLabeller/mutations/shrinkFontSizesUntilLabelsFitCanvasVertically.js
+++ theSrc/scripts/lib/d3pie/labellers/segmentLabeller/mutations/shrinkFontSizesUntilLabelsFitCanvasVertically.js
@@ -18,38 +18,42 @@ const mutationFn = ({ outerLabelSet: labelSet, invariant, canvas }) => {
   const preferredMinFontSize = extractAndThrowIfNull(invariant, 'preferredMinFontSize')
   const canvasHeight = extractAndThrowIfNull(canvas, 'height')
 
-  let labelStats = computeLabelStats(labelSet, outerPadding)
-  let heightDeficit = labelStats.totalDesiredHeight - canvasHeight
+  if (preferredMaxFontSize === preferredMinFontSize) {
+    stats.skipped = true
+  } else {
+    let labelStats = computeLabelStats(labelSet, outerPadding)
+    let heightDeficit = labelStats.totalDesiredHeight - canvasHeight
 
-  if (heightDeficit > 0) {
-    // apply increasingly aggressive font size scales, until everything is minFontSize
-    const fontSizeScaleOptions = _.range(preferredMaxFontSize, preferredMinFontSize - 1).map((newMaxFontSize, i) => {
-      return {
-        scale: d3.scale.linear().domain([0, labelSet.length]).range([newMaxFontSize, preferredMinFontSize]),
-        minFontSize: preferredMinFontSize,
-        maxFontSize: newMaxFontSize,
-        id: i,
-      }
-    })
+    if (heightDeficit > 0) {
+      // apply increasingly aggressive font size scales, until everything is minFontSize
+      const fontSizeScaleOptions = _.range(preferredMaxFontSize, preferredMinFontSize - 1).map((newMaxFontSize, i) => {
+        return {
+          scale: d3.scale.linear().domain([0, labelSet.length]).range([newMaxFontSize, preferredMinFontSize]),
+          minFontSize: preferredMinFontSize,
+          maxFontSize: newMaxFontSize,
+          id: i,
+        }
+      })
 
-    // NB note both labelSet and descendingValuesLabelSet are referencing same data items, so mods to one are reflected in the other
-    const descendingValuesLabelSet = _(labelSet).orderBy(['value'], ['desc']).value()
+      // NB note both labelSet and descendingValuesLabelSet are referencing same data items, so mods to one are reflected in the other
+      const descendingValuesLabelSet = _(labelSet).orderBy(['value'], ['desc']).value()
 
-    _(fontSizeScaleOptions).each(({ scale, minFontSize, maxFontSize, id }) => {
-      newVariants.maxFontSize = maxFontSize
-      newVariants.minFontSize = minFontSize
-      stats.lastIteration = id
-      _(descendingValuesLabelSet).each((label, i) => { label.fontSize = Math.round(scale(i)) })
-      labelStats = computeLabelStats(labelSet, outerPadding)
-      labelLogger.info(`Applying labelFontScale option ${id}: font range: [${minFontSize}:${maxFontSize}]`)
-      labelLogger.info(`New fontSizeDistribution: ${JSON.stringify(labelStats.fontSizeDistribution, {}, 2)}`)
+      _(fontSizeScaleOptions).each(({ scale, minFontSize, maxFontSize, id }) => {
+        newVariants.maxFontSize = maxFontSize
+        newVariants.minFontSize = minFontSize
+        stats.lastIteration = id
+        _(descendingValuesLabelSet).each((label, i) => { label.fontSize = Math.round(scale(i)) })
+        labelStats = computeLabelStats(labelSet, outerPadding)
+        labelLogger.info(`Applying labelFontScale option ${id}: font range: [${minFontSize}:${maxFontSize}]`)
+        labelLogger.info(`New fontSizeDistribution: ${JSON.stringify(labelStats.fontSizeDistribution, {}, 2)}`)
 
-      if (labelStats.totalDesiredHeight <= canvasHeight) {
-        labelLogger.info(`labelFontScale option(${id}):[${minFontSize}:${maxFontSize}] provided enough shrinkage. Moving on to next step`)
-        stats.completed = true
-        return terminateLoop
-      }
-    })
+        if (labelStats.totalDesiredHeight <= canvasHeight) {
+          labelLogger.info(`labelFontScale option(${id}):[${minFontSize}:${maxFontSize}] provided enough shrinkage. Moving on to next step`)
+          stats.completed = true
+          return terminateLoop
+        }
+      })
+    }
   }
 
   return {
